216d052d69bc75196165b0b0bfafca30225b2183
Geolangsat Narzary <narz0001@e.ntu.edu.sg>
2023-11-09 17:28:17 +0800

    Second rendition with image upload.

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/process', methods=['POST'])
 def process():
-    user_input = request.form['fruit_choice']
+    fruit_type = request.form['fruit_type']
 
-    if user_input.isdigit() and 1 <= int(user_input) <= 9:
-        return f"You chose fruit number {user_input}"
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        # Process the uploaded file as needed
+        # Your logic to handle the uploaded file goes here
+        return f'File uploaded successfully for {fruit_type}!'
     else:
-        return "Invalid input. Please enter a number between 1 and 9."
-
+        flash('Invalid file format. Please upload a valid image file.')
+        return redirect(request.url)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
