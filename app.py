f7a1cc042f7d71548969549a043e7795f20fe744
Geolangsat Narzary <142396058+narz0001@users.noreply.github.com>
2023-11-10 15:39:28 +0800

    Added ML Model Selection and Training based on Fruit name.

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 import os
-
 from skimage import io
 from skimage.color import rgb2gray
 from skimage.transform import resize
-
 from flask import Flask, render_template, request, redirect, flash
 from data_preparation import prepare_data
-from ml_models import train_knn_model, evaluate_knn_model
+from ml_models import train_knn_model, train_svc_model, train_rf_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 home = app.root_path
-fruit = 'bellpepper'
-categories = ['healthy', 'unhealthy']
 
-data_input, data_target = prepare_data(f"{home}{os.sep}training{os.sep}", fruit, categories)
-knn_model = train_knn_model(data_input, data_target)
-
-# Sample test data (replace this with the actual test data)
-X_test, y_test = prepare_data(f"{home}{os.sep}training{os.sep}", fruit, categories)
+# Mapping of fruits to machine learning models
+model_mapping = {
+    'Apple': 'svc',
+    'Cherry': 'svc',
+    'Grape': 'svc',
+    'Tomato': 'svc',
+    'Peach': 'svc',
+    'Strawberry': 'svc',
+    'Bellpepper': 'knn',
+    'Potato': 'rf',
+    'Corn': 'rf'
+}
 
 @app.route('/')
 def index():
@@ -28,28 +33,59 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
+        fruit_type = request.form['fruit_type']
+
+        # Ensure a valid fruit selection
+        if fruit_type not in model_mapping:
+            flash('Invalid fruit selection')
             return redirect(request.url)
 
-        file = request.files['file']
+        # Choose the appropriate fruit based on the selected option
+        fruit = fruit_type
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+        # Get the selected model name
+        model_name = model_mapping[fruit_type]
 
-        # Sample test data preparation for the uploaded file
-        uploaded_image = io.imread(file)
-        resize_img = resize(uploaded_image, (200, 200))
-        gray_image = rgb2gray(resize_img)
-        flattened_image = gray_image.flatten()
+        if model_name:
+            # Prepare data based on the selected fruit
+            data_input, data_target = prepare_data(f"{home}{os.sep}training{os.sep}", fruit, ['healthy', 'unhealthy'])
+            X_train, X_test, y_train, y_test = train_test_split(data_input, data_target, test_size=0.3, random_state=0)
 
-        # Reshape the image to match the training data
-        reshaped_image = flattened_image.reshape(1, -1)
+            if model_name == 'svc':
+                model = train_svc_model(X_train, y_train)
+            elif model_name == 'knn':
+                model = train_knn_model(X_train, y_train)
+            elif model_name == 'rf':
+                model = train_rf_model(X_train, y_train)
+            else:
+                flash('Invalid model selection')
+                return redirect(request.url)
 
-        # Predict using the KNN model
-        prediction = knn_model.predict(reshaped_image)
-        return render_template('result.html', prediction=prediction[0])
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+
+            file = request.files['file']
+
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+
+            # Sample test data preparation for the uploaded file
+            uploaded_image = io.imread(file)
+            resize_img = resize(uploaded_image, (200, 200))
+            gray_image = rgb2gray(resize_img)
+            flattened_image = gray_image.flatten()
+
+            # Reshape the image to match the training data
+            reshaped_image = flattened_image.reshape(1, -1)
+
+            # Predict using the selected model
+            prediction = model.predict(reshaped_image)
+            y_pred = model.predict(X_test)
+            accuracy = f"{accuracy_score(y_test, y_pred) * 100:.2f}%"
+
+            return render_template('result.html', prediction=prediction[0], accuracy_score=accuracy, model_name=model_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
